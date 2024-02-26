b08ceff933f3c30ec4eb2c9994729737e3cf8878
imran2024bd <imranitbd34@gmail.com>
2024-02-26 21:55:02 +0600

    javascript common concept clear

--- passby.js
+++ passby.js
@@ -6,7 +6,7 @@ let num2 = 7 ;
 function multiply( a , b) {
     // a = 27;
     const result = a * b ;
-    rrimitiveeturn result;
+    return result;
 }
 
 const output = multiply(num1 , num2);
--- task.js
+++ task.js
@@ -23,7 +23,37 @@
 // console.log(friends.find(friend => friend.length ==5));
 
 
-const product = {name: 'Laptop', model:'Yoga 3', price:49000, dusk: '512SSD'}
+// const product = {name: 'Laptop', model:'Yoga 3', price:49000, dusk: '512SSD'}
 
-const {price} = product;
-console.log(price);
+// const {price} = product;
+// console.log(price);
+
+let p='Javascript'; 
+let q=p; 
+p='React'; 
+// console.log(q);
+
+
+
+const isTrue=true;
+// console.log(!isTrue ? "hello" : "world")
+
+
+const sum =(p, q)=> {
+    p + q;
+   }
+   const result = sum(2, 3);
+//    console.log(result);
+
+// if ("2" === 2) {
+//     console.log("Inside if");
+//    } else {
+//     console.log("Inside else");
+//    }
+
+   function work(x, y = 4) {
+    return x + y;
+   }
+   console.log(work(32);
+
+
